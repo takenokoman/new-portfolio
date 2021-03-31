6615db1020019b03f3838f1876b8f914e3c6b88c
Takeru <kingdomtakeru@gmail.com>
2021-04-01 00:00:35 +0900

    Correct the text

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,11 @@
-  
+
 $('a[href^="#"]').click(function(){
   var speed = 900;
   var href = $(this).attr("href");
   var target = $(href == "#" ? "html": href);
   var position = target.offset().top;
   $('body,html').animate({
-    scrollTop:position - 60
+    scrollTop:position - 100
   }, speed, 'easeOutQuad');
   return false;
 });
